cd59766f4a3c2dd7a25f55977b398bfdee40f510
nosianuri <nurinosia@gmail.com>
2022-05-22 22:19:24 +0600

    set navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Purchase from './pages/Purchase/Purchase';
+import Navbar from './pages/Shared/Navbar';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/purchase' element={<Purchase />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Purchase/Purchase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Purchase = () => {
+    return (
+        <div>
+            <h1>This is for purchase</h1>
+        </div>
+    );
+};
+
+export default Purchase;

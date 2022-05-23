ab854ebf4e8ffcff359bd112e00b4743f1dc0acc
nosianuri <nurinosia@gmail.com>
2022-05-23 12:13:20 +0600

    set home page contact

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import camera from '../../assets/images/camera.png';
-import bg from '../../assets/images/bg.png';
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img width={550} src={camera} alt="banner"/>
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img width={550} src={camera} alt="banner" />
                 <div>
-                    <h1 class="text-6xl font-bold">Premium Equipment</h1>
-                    <p class="py-6 text-3xl text-bold">Get your driver from your camera manufacturer website exercitationem quasi. Update your camera driver automatically.</p>
+                    <h1 className="text-6xl font-bold uppercase">Premium Equipment</h1>
+                    <p className="py-6 text-3xl text-bold">Get your driver from your camera manufacturer website exercitationem quasi. Update your camera driver automatically.</p>
                 </div>
             </div>
         </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
 import HFooter from './HFooter';
 
 const Home = () => {
     return (
         <div>
            <Banner />
+           <Contact />
            <HFooter />
         </div>
     );
